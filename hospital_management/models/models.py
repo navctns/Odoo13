bcb58fccc274fbf774a99b63f78a693915f168ce
NAVEEN <navsoft18@gmail.com>
2020-04-08 20:56:20 +0530

    Add existing file

--- hospital_management/models/models.py
+++ hospital_management/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import datetime
 
 
 # class hospital_management(models.Model):
@@ -22,9 +23,14 @@ class Patient_Card(models.Model):
     _name="patient.card"
     _description="Hospital management patent card"
 
-    name=fields.Char(string="Patient Name",required=True,help="Name of the patient")
+    #name=fields.Char(string="Patient Name",required=True,help="Name of the patient")
+    name=fields.Many2one("res.partner",ondelete="set null",string="Patient",Index=True)
     dob=fields.Date()
     age=fields.Integer(string="Age of the Patient")
     address=fields.Text(required=True)
+    date=fields.Date(default=datetime.datetime.now())
+
+    #blood_group=fields.Many2one(string="blood group")
+
 
 
