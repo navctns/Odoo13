7aadaf8527ef12bee84e8d61628fd4bbe247215b
NAVEEN <navsoft18@gmail.com>
2020-04-17 20:23:00 +0530

    Add existing file

---
+++ basic_versions/TASK 1/hospital_management_patient_card_TASK 1/models/hospital_op.py
@@ -0,0 +1,62 @@
+from odoo import models, fields, api
+import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+class OP(models.Model):
+    _name="hospital.op"
+    _description="Hospital OP"
+
+    card_id = fields.Many2one("patient.card", ondelete ="set null", string="Patient Card", Index=True)
+    dob = fields.Date(string = "DOB")
+    patient_name = fields.Char(string = "Patient Name")
+    patient_age = fields.Integer(string = "Age")
+    patient_gender = fields.Char("Gender")
+    patient_blood = fields.Char("Blood Group")
+    doctor_id = fields.Many2one("hr.employee", ondelete="set null", string = "Doctor", Index=True)
+    department_id = fields.Char(string = "Department")
+    date= fields.Date(string = "Date", default = datetime.date.today())
+    token_no = fields.Char(string='Token No', required=True, copy=False, readonly=True,
+                      default='New')
+
+    consultation_type = fields.Selection([
+        ('OP', 'OP'),
+        ('IP', 'IP')
+    ], string = "Consultation Type", realated_field = 'hospital.consult.type')
+
+    @api.model
+
+    def create(self, vals):
+        if vals.get('token_no', 'New') == 'New':
+            vals['token_no'] = self.env['ir.sequence'].next_by_code(
+                'op.card') or 'New'
+        result = super(OP, self).create(vals)
+        return result
+
+    # @api.model
+    # def create(self, vals):
+    #     if vals.get('token_no', 'New') == 'New':
+    #         vals['token_no'] = self.env['ir.sequence'].next_by_id(
+    #             self,sequence_date=datetime.date.today()) or 'New'
+    #     result = super(OP, self).create(vals)
+    #     print('result',result)
+    #     return result
+
+    @api.onchange('card_id')
+
+    def _onchange_card_id(self):
+
+        for r in self:
+            r.dob = self.card_id.dob
+            r.patient_name = self.card_id.patient_id.name
+            r.patient_age = self.card_id.age
+            r.patient_gender = self.card_id.sex
+            r.patient_blood = self.card_id.blood_group
+
+            print(r.dob)
+
+    @api.onchange('doctor_id')
+    def _onchange_doctor_id(self):
+
+        self.department_id = self.doctor_id.department_id.name
---
+++ basic_versions/TASK 1/hospital_management_patient_card_TASK 1/models/models.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+# class hospital_management(models.Model):
+#     _name = 'hospital_management.hospital_management'
+#     _description = 'hospital_management.hospital_management'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.valu
+
+class PatientCard(models.Model):
+    _name="patient.card"
+    _description="Hospital management patent card"
+    _rec_name ="seq"
+
+    patient_id=fields.Many2one("res.partner",ondelete="cascade",string="Patient Name",Index=True, required=True)
+    dob=fields.Date(string="DOB", required=True)
+    age=fields.Integer(string="Age", required=True)
+    sex=fields.Selection([
+        ('male','Male'),
+        ('female','Female'),
+        ('other','Other')
+    ],string="Sex", required=True)
+    #address=fields.Text(required=True,string="Address")
+    date=fields.Date(string="OP Date",default=datetime.datetime.now())
+    phone=fields.Char(string="Telephone")
+    mobile=fields.Char(string="Mobile", required=True)
+    blood_group=fields.Selection([
+        ('A+','A+ve'),
+        ('B+','B+ve'),
+        ('O+','O+ve'),
+        ('AB+', 'AB+ve'),
+        ('A-', 'A-ve'),
+        ('B-', 'B-ve'),
+        ('O-', 'O-ve'),
+        ('AB-', 'AB-ve')
+
+    ],string="Blood Group", required=True)
+    seq = fields.Char(string='Patient Reference', required=True, copy=False, readonly=True,
+                      default='New')
+
+
+
+
+    @api.model
+    def create(self, vals):
+        if vals.get('seq', 'New') == 'New':
+            vals['seq'] = self.env['ir.sequence'].next_by_code(
+                'patient.card') or 'New'
+        result = super(PatientCard, self).create(vals)
+        return result
+
+    @api.onchange('patient_id')
+    def _onchange_patient_id(self):
+
+        self.phone=self.patient_id.phone
+        self.mobile=self.patient_id.mobile
+
+    @api.onchange('dob')
+    def _onchange_dob(self):
+        if self.dob:
+            year = self.dob.year
+            month = self.dob.month
+            day = self.dob.day
+
+            if len(str(month)) == 1:
+                month = "0" + str(month)
+            if len(str(day)) == 1:
+                day = "0" + str(day)
+
+            bday = str(year) + "-" + str(month) + "-" + str(day)
+            d1=datetime.datetime.strptime(bday,"%Y-%m-%d").date()
+            d2 = date.today()
+            self.age = relativedelta(d2,d1).years
+
+
+class Consultation(models.Model):
+
+    _name = "hospital.consult"
+    _description = "Hospital management patent consultation"
+
+    card_id = fields.Many2one('patient.card',string="Patient Card")
+    type = fields.Selection([
+        ('OP','OP'),
+        ('IP','IP')
+    ],string = "Consultation Type")
+    doctor_id = fields.Many2one('hr.employee')
+    date= fields.Date(string = "Date", default = datetime.date.today())
+    disease_id = fields.Many2one("hospital.disease", string = "Disease")
+    diagnose = fields.Text(string = "Diagnose")
+    treatement = fields.Many2one("consult.line")
+
+
+
+class Disease(models.Model):
+
+    _name = "hospital.disease"
+    _description = "Disease"
+
+    #desease_id = fields.One2many(string="Disease")
+    disease_id = fields.Char(string="Disease")
+
+class ConsultLine(models.Model):
+
+    _name = "consult.line"
+    _description = "Disease"
+
+    consult_id = fields.Char(string = "Consult Line")
+    medicine = fields.Char(string = "Medicine")
+    dose = fields.Char(string = "Dose")
+    days = fields.Integer(string = "Days")
+
+
+
+
+
+
+
+
