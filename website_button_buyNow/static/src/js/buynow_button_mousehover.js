9015a796341fa4403df8c93db22451458dde7613
NAVEEN <navsoft18@gmail.com>
2020-06-20 08:58:23 +0530

    Add existing file

--- website_button_buyNow/static/src/js/buynow_button_mousehover.js
+++ website_button_buyNow/static/src/js/buynow_button_mousehover.js
@@ -12,13 +12,31 @@ var timeout;
 //card oe_product_cart  action="/shop/cart/update"
 publicWidget.registry.WebsiteSale.include({
 
+/////////Method 1/////////////////
 //    selector: '.card oe_product_cart',
-    events: {
-        'mouseenter .o_wsale_product_grid_wrapper': 'buynowButton',
-        'mouseleave .o_wsale_product_grid_wrapper': '_onMouseLeave',
+//    events: {
+//        'mouseenter .o_wsale_product_grid_wrapper': 'buynowButton',
+//        'mouseleave .o_wsale_product_grid_wrapper': '_onMouseLeave',
+////        'click': '_onClick',
+//    },
+    //////Method 1 end////////////////
+    ////Method 2/////////////
+    //change event selector
+//            card-body p-1 oe_product_image
+//        events: {
+//        'mouseenter .card-body p-1 oe_product_image': 'buynowButton',
+//        'mouseleave .card-body p-1 oe_product_image': '_onMouseLeave',
+////        'click': '_onClick',
+//    },
+     //change event selector//////
+     /////Method 2 end//////////
+     /////////method 3////////////////
+     events: {
+        'mouseenter .oe_product': 'buynowButtonShow',
+        'mouseleave .oe_product': 'buynowButtonHide',
 //        'click': '_onClick',
-    },
-
+   },
+     ////////method 3 end////////////
 //     start: function() {
 //            console.log("pet store home page loaded");
 //        },
@@ -45,7 +63,7 @@ publicWidget.registry.WebsiteSale.include({
 //
 //        },
 
-        buynowButton: function (ev) {
+        buynowButtonShow: function (ev) {
         var self = this;
 //        this.$("#buynow").hide()
 //          this.trigger(this.$("#buynow").style.display='inline');
@@ -57,19 +75,28 @@ publicWidget.registry.WebsiteSale.include({
 //            style="display: none;"
 
 //            this.$el.find("#buynow").css('display':'inline');
-            this.$("#buy_now").css('display','inline');
+//        timeout = setTimeout(function (){
+            setTimeout(function (){
+//            this.$("#buy_now").css('display','inline');//Previous ver
 //            alert();
+            $(ev.currentTarget).find('#buy_now').css('display','inline');
+//            $(ev.currentTarget).find('.buynow').css('display','inline');
             console.log('enter')
 //        }
-
+         }, 300);
         },
 
-        _onMouseLeave: function (ev) {
-        var self = this;
-//        if ($(ev.currentTarget).attr('class') != 'card oe_product_cart'){
-//            this.$(".buynow").hide();
-//        }
-            this.$("#buy_now").css('display','none');
+        buynowButtonHide: function (ev) {
+        setTimeout(function (){
+            var self = this;
+    //        if ($(ev.currentTarget).attr('class') != 'card oe_product_cart'){
+    //            this.$(".buynow").hide();
+    //        }
+    //            this.$("#buy_now").css('display','none');//Prev ver
+                  $(ev.currentTarget).find('#buy_now').css('display','none');
+//                    $(ev.currentTarget).find('.buynow').css('display','none');
+
+            },300);
         },
 
 
