3cf36818011ba9cbc060a83c53b9689c26280641
NAVEEN <navsoft18@gmail.com>
2020-06-26 14:52:58 +0530

    Add existing file

--- website_mrp_orders/models/mrp_partner.py
+++ website_mrp_orders/models/mrp_partner.py
@@ -8,7 +8,7 @@ class MrpAddPartner(models.Model):
 
     #check company is true(Test)
     partner_id = fields.Many2one('res.partner', string='Partner', check_company=True)
-    mrp_order_count = fields.Integer('mrp order count',default = lambda self: self.env['mrp.production'].search_count([('partner_id','=',self.env.user.id)]))
+    mrp_order_count = fields.Integer('mrp order count',default = lambda self: self.env['mrp.production'].search_count([('partner_id','=',self.env.user.id)]) if len(self)!=0 else 0)
     active = fields.Boolean('Active', default = True)
 
     # @api.depends('moderator_ids')
